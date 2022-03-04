4d0e5f2eae38cdeaf06b1746e119bff4e02c7d08
dilrong <dilrong@dilrong.com>
2022-03-05 00:06:29 +0900

    feat: checkholder

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
+import msg from "msg.js";
 import { SkyRouter } from "skyrouter";
+import superagent from "superagent";
+import BrowserInfo from "./BrowserInfo";
+
 import CheckHolder from "./view/CheckHolder";
 
 (async () => {
+    msg.language = BrowserInfo.language;
+    msg.parseCSV((await superagent.get("/msg.csv")).text);
+
     SkyRouter.route("checkholder", CheckHolder);
 
     if (sessionStorage.__spa_path) {
--- src/view/CheckHolder.ts
+++ src/view/CheckHolder.ts
@@ -4,6 +4,7 @@ import { View, ViewParams } from "skyrouter";
 import superagent from "superagent";
 import DiscordUserInfo from "../DiscordUserInfo";
 import Wallet from "../klaytn/Wallet";
+// import Layout from "./Layout";
 
 export default class CheckHolder implements View {
 
@@ -12,18 +13,20 @@ export default class CheckHolder implements View {
     public discordUser: DiscordUserInfo | undefined;
 
     constructor() {
-        BodyNode.append(this.container = el(".check-holder-view",
-            el("header",
-                el("h1", msg("HOLDER_CHECK_TITLE")),
-                el("h2", msg("HOLDER_CHECK_DESC"))
-            ),
-            el("article",
-                el("img", { src: "/images/shared/img/earth.png", alt: "earth" }),
-                el("a.discord-login-button", msg("HOLDER_CHECK_BUTTON"), {
-                    href: "https://discord.com/api/oauth2/authorize?client_id=939799459720728606&redirect_uri=https%3A%2F%2Fsigor.com%2Fcheckholder&response_type=code&scope=identify",
-                }),
-            ),
-        ));
+        // Layout.current.title = msg("HOME_TITLE");
+        BodyNode.append(
+            this.container = el(".check-holder-view",
+                el("header",
+                    el("h1", msg("HOLDER_CHECK_TITLE")),
+                    el("h2", msg("HOLDER_CHECK_DESC"))
+                ),
+                el("article",
+                    el("img", { src: "/images/view/check-holder/housedeed.png" }),
+                    el("a.discord-login-button", msg("HOLDER_CHECK_BUTTON"), {
+                        href: "https://discord.com/api/oauth2/authorize?client_id=939799459720728606&redirect_uri=https%3A%2F%2Fklu.bs%2Fcheckholder&response_type=code&scope=identify",
+                    }),
+                ),
+            ));
         this.checkDiscordLogin();
     }
 
@@ -32,7 +35,7 @@ export default class CheckHolder implements View {
         let code: string | undefined = new URLSearchParams(window.location.search).get("code")!;
         if (code !== null) {
             try {
-                await superagent.get("https://api.sigor.com/discord/token").query({
+                await superagent.get("https://api.klu.bs/discord/token").query({
                     code,
                     redirect_uri: `${window.location.protocol}//${window.location.host}/checkholder`,
                 });
@@ -46,7 +49,7 @@ export default class CheckHolder implements View {
 
         if (code !== undefined) {
             try {
-                const result = await superagent.get("https://api.sigor.com/discord/me").query({ code });
+                const result = await superagent.get("https://api.klu.bs/discord/me").query({ code });
                 this.discordUser = result.body;
                 this.checkWalletConnected(code);
             } catch (error) {
@@ -66,7 +69,7 @@ export default class CheckHolder implements View {
             const signResult = await Wallet.signMessage(message);
 
             try {
-                const result = await fetch("https://api.sigor.com/checkholder", {
+                const result = await fetch("https://api.klu.bs/checkholder", {
                     method: "POST",
                     body: JSON.stringify({
                         code,
