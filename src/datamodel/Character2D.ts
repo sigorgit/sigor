02a7205ce552e4cff7898e526cadbc198adec62b
Sim Young Jae <hanul@hanul.me>
2022-07-03 16:06:40 +0900

    g

--- src/datamodel/Character2D.ts
+++ src/datamodel/Character2D.ts
@@ -1,18 +1,22 @@
 export interface Sprite {
     image?: string,
+
     crop_x?: number,
     crop_y?: number,
     crop_width?: number,
     crop_height?: number,
+
     frame_width?: number,
     frame_height?: number,
     fps?: number,
+
     center_x?: number,
     center_y?: number,
 }
 
 export interface DirectionSprites {
     set_image?: string,
+
     top?: Sprite,
     bottom?: Sprite,
     left?: Sprite,
@@ -22,6 +26,7 @@ export interface DirectionSprites {
 export default interface Character2D {
     set_image?: string,
     scale?: number,
+
     stand?: DirectionSprites,
     walk?: DirectionSprites,
     run?: DirectionSprites,
